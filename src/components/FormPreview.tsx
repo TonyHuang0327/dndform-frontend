4b0ccbaefc3b9dc2148e94d68c3332d1cddcccff
Tony <Tony23046176@gmail.com>
2026-03-17 14:42:23 +0800

    fix: add keydown to adjust span

--- src/components/FormPreview.tsx
+++ src/components/FormPreview.tsx
@@ -92,6 +92,9 @@ export default function FormPreview({ fields }: FormPreviewProps) {
                       "& .MuiOutlinedInput-notchedOutline": {
                         border: "none",
                       },
+                      "& .Mui-focused":{
+                        backgroundColor: "aliceblue",
+                      }
                     }}
                   />
                 </Grid>
